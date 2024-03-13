1e04861806b0f6ed57865797a60c0085fcce01cb
skearya <77034153+skearya@users.noreply.github.com>
2024-03-12 22:53:50 -0400

    basics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,65 @@
 import type { Component } from 'solid-js';
+import type { ClientMessage, ServerMessage } from './types/messages';
+import { createSignal, Switch, Match, For, onCleanup } from 'solid-js';
+import { match } from 'ts-pattern';
 
 const App: Component = () => {
-	return <p class="py-20 text-center text-4xl text-green-700">Hello tailwind!</p>;
+	const [state, setState] = createSignal<'connecting' | 'error' | 'lobby' | 'game'>('connecting');
+	const [messages, setMessages] = createSignal<Array<string>>([]);
+	const [input, setInput] = createSignal<string>('');
+
+	const room = prompt('room', 'one');
+	const username = prompt('username', 'skeary');
+
+	const params = new URLSearchParams({ username: username });
+	const socket = new WebSocket(
+		`${import.meta.env.PUBLIC_SERVER}/rooms/${room}?${params.toString()}`
+	);
+
+	socket.addEventListener('message', (event) => {
+		const message: ServerMessage = JSON.parse(event.data);
+	});
+
+	function sendMessage(data: ClientMessage) {
+		socket.send(JSON.stringify(data));
+	}
+
+	onCleanup(() => {
+		socket.close();
+	});
+
+	return (
+		<Switch>
+			<Match when={state() === 'connecting'}>
+				<h1>connecting</h1>
+			</Match>
+			<Match when={state() === 'error'}>
+				<h1>we errored</h1>
+			</Match>
+			<Match when={state() === 'lobby'}>
+				<h1>ready players: </h1>
+
+				<input
+					type="text"
+					onKeyDown={(event) => {
+						if (event.key === 'Enter') {
+							sendMessage({
+								type: 'chatMessage',
+								content: event.currentTarget.value
+							});
+						}
+					}}
+				/>
+
+				<ul class="list-item">
+					<For each={messages()}>{(message, _) => <li>{message}</li>}</For>
+				</ul>
+			</Match>
+			<Match when={state() === 'game'}>
+				<h1>game</h1>
+			</Match>
+		</Switch>
+	);
 };
 
-export default App;
+export { App };
---
+++ client/src/types/messages.ts
@@ -0,0 +1,118 @@
+export type ClientMessage =
+	| { type: 'ready' }
+	| { type: 'unready' }
+	| { type: 'chatMessage'; content: string }
+	| { type: 'input'; input: string }
+	| { type: 'guess'; word: string };
+
+export type ServerMessage =
+	| {
+			type: 'roomInfo';
+			uuid: string;
+			state: RoomState;
+	  }
+	| {
+			type: 'serverMessage';
+			content: string;
+	  }
+	| {
+			type: 'chatMessage';
+			author: string;
+			content: string;
+	  }
+	| {
+			type: 'readyPlayers';
+			players: Array<PlayerInfo>;
+	  }
+	| {
+			type: 'startingCountdown';
+			state: CountdownState;
+	  }
+	| {
+			type: 'gameStarted';
+			rejoinToken?: string;
+			prompt: string;
+			turn: string;
+			players: Array<PlayerData>;
+	  }
+	| {
+			type: 'playerUpdate';
+			uuid: string;
+			state: PlayerUpdate;
+	  }
+	| {
+			type: 'inputUpdate';
+			uuid: string;
+			input: string;
+	  }
+	| {
+			type: 'invalidWord';
+			uuid: string;
+			reason: InvalidWordReason;
+	  }
+	| {
+			type: 'newPrompt';
+			lifeChange: number;
+			prompt: string;
+			turn: string;
+	  }
+	| {
+			type: 'gameEnded';
+			winner: string;
+	  };
+
+type RoomState =
+	| {
+			type: 'lobby';
+			ready: Array<PlayerInfo>;
+			startingCountdown?: number;
+	  }
+	| {
+			type: 'inGame';
+			prompt: string;
+			turn: string;
+			players: Array<PlayerData>;
+			usedLetters?: Array<string>;
+	  };
+
+export type PlayerInfo = {
+	uuid: string;
+	username: string;
+};
+
+export type PlayerData = {
+	uuid: string;
+	username: string;
+	input: string;
+	lives: number;
+	disconnected: boolean;
+};
+
+type PlayerUpdate =
+	| {
+			type: 'disconnected';
+	  }
+	| {
+			type: 'reconnected';
+			username: string;
+	  };
+
+type CountdownState =
+	| {
+			type: 'inProgress';
+			timeLeft: number;
+	  }
+	| {
+			type: 'stopped';
+	  };
+
+type InvalidWordReason =
+	| {
+			type: 'promptNotIn';
+	  }
+	| {
+			type: 'notEnglish';
+	  }
+	| {
+			type: 'alreadyUsed';
+	  };
