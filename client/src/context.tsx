26c3bcc6f2d2a1321b080ee57f707d9382a2cd20
skearya <77034153+skearya@users.noreply.github.com>
2024-03-15 00:54:24 -0400

    create context.tsx

---
+++ client/src/context.tsx
@@ -0,0 +1,39 @@
+import { ParentComponent, createContext, createSignal } from 'solid-js';
+import { createStore } from 'solid-js/store';
+import { ConnectionData, GameData, LobbyData } from './types/stores';
+
+function makeContext() {
+	const [state, setState] = createSignal<'connecting' | 'error' | 'lobby' | 'game'>('connecting');
+
+	const [connection, setConnection] = createStore<ConnectionData>({
+		room: prompt('room', 'one')!,
+		username: prompt('username', 'skeary')!,
+		uuid: '',
+		chatMessages: []
+	});
+
+	const [lobby, setLobby] = createStore<LobbyData>({
+		readyPlayers: [],
+		previousWinner: null,
+		startingCountdown: null
+	});
+
+	const [game, setGame] = createStore<GameData>({
+		players: [],
+		currentTurn: '',
+		prompt: '',
+		usedLetters: new Set(),
+		input: ''
+	});
+
+	return [
+		{ state, connection, lobby, game },
+		{ setState, setConnection, setLobby, setGame }
+	] as const;
+}
+
+const Context = createContext<ReturnType<typeof makeContext>>(makeContext());
+
+export const ContextProvider: ParentComponent = (props) => {
+	return <Context.Provider value={makeContext()}>{props.children}</Context.Provider>;
+};
