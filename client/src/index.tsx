97f7fa19dc063f1ef3b7ebe3a68935d9d143cf88
skearya <77034153+skearya@users.noreply.github.com>
2024-03-18 00:13:23 -0400

    theme and colors

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,10 @@ import { render } from 'solid-js/web';
 
 import App from './App';
 
+document.documentElement.classList[
+	window.matchMedia('(prefers-color-scheme: light)').matches ? 'remove' : 'add'
+]('dark');
+
 const root = document.getElementById('root');
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -6,71 +6,14 @@ const config: Config = {
 	theme: {
 		extend: {
 			colors: {
-				text: {
-					50: 'hsl(var(--text-50))',
-					100: 'hsl(var(--text-100))',
-					200: 'hsl(var(--text-200))',
-					300: 'hsl(var(--text-300))',
-					400: 'hsl(var(--text-400))',
-					500: 'hsl(var(--text-500))',
-					600: 'hsl(var(--text-600))',
-					700: 'hsl(var(--text-700))',
-					800: 'hsl(var(--text-800))',
-					900: 'hsl(var(--text-900))',
-					950: 'hsl(var(--text-950))'
-				},
-				background: {
-					50: 'hsl(var(--background-50))',
-					100: 'hsl(var(--background-100))',
-					200: 'hsl(var(--background-200))',
-					300: 'hsl(var(--background-300))',
-					400: 'hsl(var(--background-400))',
-					500: 'hsl(var(--background-500))',
-					600: 'hsl(var(--background-600))',
-					700: 'hsl(var(--background-700))',
-					800: 'hsl(var(--background-800))',
-					900: 'hsl(var(--background-900))',
-					950: 'hsl(var(--background-950))'
-				},
-				primary: {
-					50: 'hsl(var(--primary-50))',
-					100: 'hsl(var(--primary-100))',
-					200: 'hsl(var(--primary-200))',
-					300: 'hsl(var(--primary-300))',
-					400: 'hsl(var(--primary-400))',
-					500: 'hsl(var(--primary-500))',
-					600: 'hsl(var(--primary-600))',
-					700: 'hsl(var(--primary-700))',
-					800: 'hsl(var(--primary-800))',
-					900: 'hsl(var(--primary-900))',
-					950: 'hsl(var(--primary-950))'
-				},
-				secondary: {
-					50: 'hsl(var(--secondary-50))',
-					100: 'hsl(var(--secondary-100))',
-					200: 'hsl(var(--secondary-200))',
-					300: 'hsl(var(--secondary-300))',
-					400: 'hsl(var(--secondary-400))',
-					500: 'hsl(var(--secondary-500))',
-					600: 'hsl(var(--secondary-600))',
-					700: 'hsl(var(--secondary-700))',
-					800: 'hsl(var(--secondary-800))',
-					900: 'hsl(var(--secondary-900))',
-					950: 'hsl(var(--secondary-950))'
-				},
-				accent: {
-					50: 'hsl(var(--accent-50))',
-					100: 'hsl(var(--accent-100))',
-					200: 'hsl(var(--accent-200))',
-					300: 'hsl(var(--accent-300))',
-					400: 'hsl(var(--accent-400))',
-					500: 'hsl(var(--accent-500))',
-					600: 'hsl(var(--accent-600))',
-					700: 'hsl(var(--accent-700))',
-					800: 'hsl(var(--accent-800))',
-					900: 'hsl(var(--accent-900))',
-					950: 'hsl(var(--accent-950))'
-				}
+				text: 'var(--text)',
+				background: 'var(--background)',
+				primary: 'var(--primary)',
+				secondary: 'var(--secondary)',
+				accent: 'var(--accent)'
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif']
 			}
 		}
 	},
