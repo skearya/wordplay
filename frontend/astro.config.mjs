6512c40e083eb71cc9046b6b28242d3cf5976aa8
skearya <77034153+skearya@users.noreply.github.com>
2024-06-13 16:22:03 -0400

    csr

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,9 +2,14 @@ import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
