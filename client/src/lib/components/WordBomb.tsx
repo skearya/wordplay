63ca91bcb53899d0ac98b8a07a94e5bbbd6810c7
skearya <x@example.com>
2024-08-20 01:21:37 -0400

    animate overlay yayyyaaa

--- client/src/lib/components/WordBomb.tsx
+++ client/src/lib/components/WordBomb.tsx
@@ -1,4 +1,4 @@
-import { Accessor, For, onMount, Show } from "solid-js";
+import { Accessor, createEffect, For, on, onCleanup, onMount, Show } from "solid-js";
 import { SetStoreFunction } from "solid-js/store";
 import { Input } from "~/lib/components/ui/Input";
 import { useEvents } from "~/lib/events";
@@ -9,6 +9,7 @@ import { getClient } from "~/lib/utils";
 
 const green = "rgb(98 226 151)";
 const red = "rgb(220 38 38)";
+const easing = "cubic-bezier(0.33, 1, 0.68, 1)";
 
 export function WordBomb({
   sendMsg,
@@ -22,8 +23,9 @@ export function WordBomb({
   setState: SetStoreFunction<State>;
 }) {
   let inputElement!: HTMLInputElement;
-  let bombOverlayElement!: HTMLDivElement;
+  let bombOverlayElement!: HTMLInputElement;
 
+  let overlayPosSet = false;
   const [game, setGame] = [
     state as Accessor<WordBombState>,
     setState as SetStoreFunction<WordBombState>,
@@ -31,8 +33,8 @@ export function WordBomb({
 
   const animateInput = (correct: boolean) => {
     inputElement.animate(
-      { borderColor: [correct ? green : red, "rgb(255 255 255)"] },
-      { duration: 800 },
+      { borderColor: [correct ? green : red, "rgb(255 255 255 / 0.1)"] },
+      { easing, duration: 800 },
     );
   };
 
@@ -42,13 +44,47 @@ export function WordBomb({
     if (playerElement) {
       playerElement.animate(
         { color: [correct ? green : red, "rgb(255 255 255)"] },
-        { duration: 800 },
+        { easing, duration: 800 },
       );
+    }
+  };
+
+  const animateOverlay = () => {
+    const playerElement = document.getElementById(game().turn);
+
+    if (playerElement) {
+      const rect = playerElement.getBoundingClientRect();
+      const padding = 35;
+
+      if (!overlayPosSet) {
+        bombOverlayElement.style.top = `${rect.top - padding / 2}px`;
+        bombOverlayElement.style.left = `${rect.left - padding / 2}px`;
+        bombOverlayElement.style.width = `${rect.width + padding}px`;
+        bombOverlayElement.style.height = `${rect.height + padding}px`;
+
+        overlayPosSet = true;
+      } else {
+        bombOverlayElement.animate(
+          {
+            top: `${rect.top - padding / 2}px`,
+            left: `${rect.left - padding / 2}px`,
+            width: `${rect.width + padding}px`,
+            height: `${rect.height + padding}px`,
+          },
+          { easing, fill: "forwards", duration: 600 },
+        );
+      }
     }
   };
 
   useEvents({
     WordBombInput: (data) => {
+      const prevLen = game().players.find((player) => player.uuid === data.uuid)!.input.length;
+
+      if ((prevLen == 0 && data.input.length >= 1) || (prevLen >= 1 && data.input.length === 0)) {
+        requestAnimationFrame(animateOverlay);
+      }
+
       setGame("players", (player) => player.uuid === data.uuid, "input", data.input);
     },
     WordBombInvalidGuess: (data) => {
@@ -90,23 +126,17 @@ export function WordBomb({
     },
   });
 
+  createEffect(on(() => game().turn, animateOverlay));
+
   onMount(() => {
     if (game().turn === room().uuid) {
       inputElement.focus();
     }
-
-    // const playerElement = document.getElementById(game().players[0]!.uuid!);
-
-    // if (playerElement) {
-    //   const rect = playerElement.getBoundingClientRect();
-    //   const paddingX = 30;
-    //   const paddingY = 28;
-    //   bombOverlayElement.style.top = `${rect.top - paddingY / 2}px`;
-    //   bombOverlayElement.style.left = `${rect.left - paddingX / 2}px`;
-    //   bombOverlayElement.style.height = `${rect.height + paddingY}px`;
-    //   bombOverlayElement.style.width = `${rect.width + paddingX}px`;
-    // }
   });
+
+  window.addEventListener("resize", animateOverlay);
+
+  onCleanup(() => window.removeEventListener("resize", animateOverlay));
 
   return (
     <main class="mt-[78px] flex h-[calc(100vh_-_78px)] flex-col justify-start">
@@ -117,14 +147,17 @@ export function WordBomb({
         {game().prompt}
       </div>
       <div class="flex h-full w-full items-center justify-around">
-        <For each={game().players}>
-          {(player) => <Player room={room} player={player} active={player.uuid === game().turn} />}
-        </For>
+        <For each={game().players}>{(player) => <Player room={room} player={player} />}</For>
+      </div>
+      <div ref={bombOverlayElement} class="absolute rounded-xl border border-dark-green">
+        <div class="absolute bottom-1.5 left-1.5 -translate-x-1/2 translate-y-1/2">
+          <SmallBomb />
+        </div>
       </div>
       <Input
         ref={inputElement}
         size="lg"
-        class="absolute bottom-6 left-1/2 -translate-x-1/2 transition-all duration-[800ms] ease-[ease] focus-visible:border-foreground"
+        class="absolute bottom-6 left-1/2 -translate-x-1/2 focus-visible:border-white/10"
         placeholder={`a word containing ${game().prompt}`}
         maxlength="35"
         disabled={game().turn !== room().uuid}
@@ -136,22 +169,14 @@ export function WordBomb({
   );
 }
 
-function Player({
-  room,
-  player,
-  active,
-}: {
-  room: Accessor<Room>;
-  player: WordBombPlayerData;
-  active: boolean;
-}) {
+function Player({ room, player }: { room: Accessor<Room>; player: WordBombPlayerData }) {
   const client = () => getClient(room(), player.uuid)!;
 
   return (
     <div
       id={player.uuid}
       classList={{ "opacity-50": client().disconnected }}
-      class="relative flex h-min flex-col items-center gap-y-2.5 text-xl transition-opacity"
+      class="flex flex-col items-center gap-y-2.5 text-xl transition-opacity"
     >
       <div class="flex items-center gap-x-4">
         <img
@@ -173,16 +198,8 @@ function Player({
           </div>
         </div>
       </div>
-      <p>{player.input}</p>
-      <Show when={active}>
-        <div
-          id="bomb-overlay"
-          class="absolute h-[calc(100%_+_40px)] w-[calc(100%_+_40px)] -translate-y-[20px] rounded-xl border border-dark-green"
-        >
-          <div class="absolute bottom-1.5 left-1.5 -translate-x-1/2 translate-y-1/2">
-            <SmallBomb />
-          </div>
-        </div>
+      <Show when={player.input.length !== 0}>
+        <p class="quick-fade-in">{player.input}</p>
       </Show>
     </div>
   );
