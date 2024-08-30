5f822a707837613f9ac847cfa1790d383d629772
skearya <x@example.com>
2024-08-30 18:23:06 -0400

    dont ship deps in frontend builder

--- build-containers.sh
+++ build-containers.sh
@@ -1,2 +1,2 @@
-docker buildx build --platform linux/amd64 --push -t ghcr.io/skearya/wordplay-frontend-builder:latest client
-docker buildx build --platform linux/amd64 --push -t ghcr.io/skearya/wordplay-server:latest server
+docker buildx build --platform linux/amd64 --push -t ghcr.io/skearya/wordplay-frontend-builder client
+docker buildx build --platform linux/amd64 --push -t ghcr.io/skearya/wordplay-server server
