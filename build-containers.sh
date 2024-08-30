33c4003631eb0f58266a9cca31257460028b461b
skearya <x@example.com>
2024-08-30 11:34:24 -0400

    build containers for x64 instead of arm

--- build-containers.sh
+++ build-containers.sh
@@ -1,5 +1,2 @@
-docker build -t ghcr.io/skearya/wordplay-frontend-builder:latest client
-docker push ghcr.io/skearya/wordplay-frontend-builder:latest
-
-docker build -t ghcr.io/skearya/wordplay-server:latest server
-docker push ghcr.io/skearya/wordplay-server:latest
+docker buildx build --platform linux/amd64 --push -t ghcr.io/skearya/wordplay-frontend-builder:latest client
+docker buildx build --platform linux/amd64 --push -t ghcr.io/skearya/wordplay-server:latest server
