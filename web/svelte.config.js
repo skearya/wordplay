87254c2841b360b9b9193567f376242c8e23b882
skearya <77034153+skearya@users.noreply.github.com>
2025-08-12 02:45:58 -0400

    workspace

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			'@bindings/*': '../bindings/*'
+			'@bindings/*': '../crates/server/bindings/*'
 		}
 	}
 };
