cdfd32f8be3fd5862bc45e357dbfeb354fdc37da
skearya <77034153+skearya@users.noreply.github.com>
2025-08-12 20:53:55 -0400

    discard wasm core, FOR NOW

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
-import wasm from 'vite-plugin-wasm';
-import topLevelAwait from 'vite-plugin-top-level-await';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait(), tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()]
 });
