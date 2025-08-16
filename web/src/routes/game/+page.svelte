a7aba70bd0a3035131b3b4b71e22dc96a3be4657
skearya <77034153+skearya@users.noreply.github.com>
2025-08-16 17:16:31 -0400

    sort imports, fix canvas width

---
+++ web/src/routes/game/+page.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+	import type { ServerMessage } from '@bindings/ServerMessage';
+	import type { SocketParams } from '@bindings/SocketParams';
+	import type { ComponentProps } from 'svelte';
+	import { onMount } from 'svelte';
+	import Wordplay from '$lib/components/Wordplay.svelte';
+	import { rootEmitter } from '$lib/events';
+	import { unreachable } from '$lib/utils';
+
+	const { data }: PageProps = $props();
+
+	type State =
+		| { kind: 'loading' }
+		| { kind: 'connected' }
+		| { kind: 'ready'; props: ComponentProps<typeof Wordplay> }
+		| { kind: 'error' };
+
+	let state = $state<State>({ kind: 'loading' });
+	let socket: WebSocket | undefined;
+
+	onMount(() => {
+		const room = 'one';
+
+		const params: SocketParams = {
+			username: 'Client',
+			rejoinToken: null
+		};
+
+		const urlParams = new URLSearchParams(
+			Object.entries(params).filter((param): param is [string, string] => param[1] !== null)
+		);
+
+		socket = new WebSocket(`ws://localhost:3000/${room}?${urlParams}`);
+
+		socket.addEventListener('open', () => {
+			console.log('Connected');
+			state = { kind: 'connected' };
+		});
+
+		socket.addEventListener('message', (e) => {
+			const message = JSON.parse(e.data) as ServerMessage;
+
+			if (import.meta.env.DEV) {
+				console.log('Message', message, e.data);
+			}
+
+			if (message.kind === 'general' && message.data.kind === 'info') {
+				state = {
+					kind: 'ready',
+					props: {
+						info: message.data,
+						sendMsg: (message) => socket!.send(JSON.stringify(message))
+					}
+				};
+
+				return;
+			}
+
+			rootEmitter.emit(message);
+		});
+
+		socket.addEventListener('error', (e) => {
+			console.log('WebSocket error', e);
+		});
+
+		socket.addEventListener('close', (e) => {
+			console.log('WebSocket closed', e);
+			state = { kind: 'error' };
+		});
+
+		return () => socket?.close();
+	});
+
+	$effect(() => {
+		if (state.kind === 'connected') {
+			const id = setTimeout(() => {
+				if (state.kind === 'connected') {
+					socket?.close();
+					state = { kind: 'error' };
+				}
+			}, 5000);
+
+			return () => clearTimeout(id);
+		}
+	});
+</script>
+
+{#if state.kind === 'loading'}
+	<h1>Loading</h1>
+{:else if state.kind === 'connected'}
+	<h1>Loading (established connection)</h1>
+{:else if state.kind === 'ready'}
+	<Wordplay {...state.props} />
+{:else if state.kind === 'error'}
+	<h1>Error</h1>
+{:else if state satisfies never}
+	{unreachable(state)}
+{/if}
