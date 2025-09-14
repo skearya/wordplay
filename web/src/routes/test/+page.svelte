2c24d9fd8c4f382b746f4aa88401ebc0018ced56
skearya <77034153+skearya@users.noreply.github.com>
2025-09-14 02:48:12 -0400

    homepage

--- web/src/routes/test/+page.svelte
+++ web/src/routes/test/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
-	import { fade, scale, slide } from 'svelte/transition';
+	import type { SVGAttributes } from 'svelte/elements';
+	import { getAbortSignal } from 'svelte';
+	import { flip } from 'svelte/animate';
+	import { blur, fade, fly, scale, slide } from 'svelte/transition';
 	import crownSvg from '$lib/assets/crown.svg';
 	import grid2Svg from '$lib/assets/grid2.svg';
 	import slashSvg from '$lib/assets/slash.svg';
@@ -9,10 +12,25 @@
 
 	let unreadMessages = $state(0);
 
+	// let readyPlayers = $state([crypto.randomUUID(), crypto.randomUUID(), crypto.randomUUID()]);
+	let readyPlayers = $state<string[]>([]);
+
 	$effect(() => {
 		setInterval(() => {
 			unreadMessages += 1;
-		}, 2000);
+		}, 1500);
+
+		document.addEventListener(
+			'keydown',
+			(e) => {
+				if (e.key === '=') {
+					readyPlayers.push(crypto.randomUUID());
+				} else if (e.key === '-') {
+					readyPlayers.pop();
+				}
+			},
+			{ signal: getAbortSignal() }
+		);
 	});
 </script>
 
@@ -54,11 +72,112 @@
 	</svg>
 {/snippet}
 
+{#snippet doubleRightArrowIcon()}
+	<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<g clip-path="url(#clip0_148_131)">
+			<mask
+				id="mask0_148_131"
+				style="mask-type:luminance"
+				maskUnits="userSpaceOnUse"
+				x="0"
+				y="0"
+				width="48"
+				height="48"
+			>
+				<path d="M48 48V0L0 0V48H48Z" fill="white" />
+			</mask>
+			<g mask="url(#mask0_148_131)">
+				<path
+					d="M0.5 46.7988V1.20117L23.4512 24L0.5 46.7988ZM24.3389 46.7988L24.3389 1.20117L47.291 24L24.3389 46.7988Z"
+					stroke="#475D50"
+				/>
+			</g>
+		</g>
+		<defs>
+			<clipPath id="clip0_148_131">
+				<rect width="48" height="48" fill="white" transform="matrix(0 1 -1 0 48 0)" />
+			</clipPath>
+		</defs>
+	</svg>
+{/snippet}
+
+{#snippet downArrowIcon()}
+	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M21.1695 6L22.5 7.182L12 18L1.5 7.182L2.8305 6L12 15.447L21.1695 6Z" fill="#8BA698" />
+	</svg>
+{/snippet}
+
+{#snippet wordBombIcon(props: SVGAttributes<SVGSVGElement>)}
+	<svg
+		width="901"
+		height="916"
+		viewBox="0 0 901 916"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		{...props}
+	>
+		<g style="mix-blend-mode:color-dodge">
+			<path
+				d="M169.563 275.312C219.15 246.154 276.72 229.465 338.126 229.465C524.314 229.465 675.251 382.898 675.251 572.163C675.251 761.428 524.314 914.861 338.126 914.861C151.936 914.861 1 761.428 1 572.163C1 509.742 17.4171 451.223 46.1016 400.814"
+				stroke="url(#paint0_linear_148_156)"
+				stroke-width="1.5"
+				stroke-linecap="round"
+			/>
+			<path
+				d="M675.252 229.465L585.352 320.851"
+				stroke="url(#paint1_linear_148_156)"
+				stroke-width="1.5"
+				stroke-linecap="round"
+			/>
+			<path
+				d="M719.351 17.137C727.698 -4.37899 757.653 -4.37899 766 17.137L795.416 92.9536C797.964 99.5224 803.08 104.722 809.544 107.313L884.125 137.215C905.292 145.7 905.292 176.151 884.125 184.637L809.544 214.538C803.08 217.129 797.964 222.329 795.416 228.898L766 304.714C757.653 326.23 727.698 326.23 719.351 304.714L689.936 228.898C687.387 222.329 682.272 217.129 675.808 214.538L601.227 184.637C580.06 176.151 580.06 145.7 601.227 137.215L675.808 107.313C682.272 104.722 687.387 99.5224 689.936 92.9536L719.351 17.137Z"
+				stroke="url(#paint2_linear_148_156)"
+				stroke-width="1.5"
+			/>
+		</g>
+		<defs>
+			<linearGradient
+				id="paint0_linear_148_156"
+				x1="338.126"
+				y1="229.465"
+				x2="338.126"
+				y2="914.861"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#FAE1DD" />
+				<stop offset="1" stop-color="#E9B8FF" />
+			</linearGradient>
+			<linearGradient
+				id="paint1_linear_148_156"
+				x1="630.302"
+				y1="229.465"
+				x2="630.302"
+				y2="320.851"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#FAE1DD" />
+				<stop offset="1" stop-color="#E9B8FF" />
+			</linearGradient>
+			<linearGradient
+				id="paint2_linear_148_156"
+				x1="742.676"
+				y1="1"
+				x2="742.676"
+				y2="320.851"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop stop-color="#FAE1DD" />
+				<stop offset="1" stop-color="#E9B8FF" />
+			</linearGradient>
+		</defs>
+	</svg>
+{/snippet}
+
 <main
 	style="background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(246, 245, 180, 0.08) 100%), #040605"
-	class="flex h-screen flex-col"
+	class="flex h-screen flex-col overflow-y-hidden"
 >
-	<nav class="flex items-center justify-between p-6">
+	<nav class="flex items-center justify-between p-5">
 		{@render wordplayLogo()}
 		<div class="flex items-center gap-x-4">
 			<div class="flex -space-x-6">
@@ -80,8 +199,10 @@
 			{@render settingsIcon()}
 		</div>
 	</nav>
-	<div class="flex h-full items-stretch gap-4 p-4 pt-0">
-		<section class="flex-[30%] border border-[#F3ACFF] bg-[#F3ACFF]/15">
+	<div class="flex flex-1 items-stretch gap-4 overflow-y-hidden p-4 pt-0">
+		<section
+			class="flex flex-[30%] flex-col overflow-y-clip border border-[#F3ACFF] bg-[#F3ACFF]/15"
+		>
 			<div
 				style={`background: url("${grid2Svg}"), #F3ACFF;`}
 				class="background-scroll p-4 pt-16 text-black"
@@ -98,7 +219,7 @@
 				</div>
 				<h1 class="text-xl"><b>skeary</b> won the game!</h1>
 			</div>
-			<div class="p-4">
+			<div class="flex flex-1 flex-col overflow-y-hidden p-4">
 				<div class="mb-4 flex items-center justify-between gap-x-8 px-8">
 					<div class="text-center">
 						<h1 style="font-family: 'PP Editorial New';" class="text-4xl text-[#F6F5B4]">21</h1>
@@ -110,11 +231,11 @@
 						<p>Words Used</p>
 					</div>
 				</div>
-				<div>
+				<div class="flex flex-1 flex-col overflow-y-hidden">
 					<div class="flex items-center -space-x-1 text-nowrap">
 						{#each ['Fastest Guess', 'Longest Word', 'Word Length', 'WPM'] as text, i}
 							<button
-								style={`z-index: ${3 - i};`}
+								style={`z-index: ${4 - i};`}
 								class={[
 									i === 0
 										? 'border-[#E9B8FF] bg-[#E9B8FF] text-black'
@@ -126,11 +247,14 @@
 							</button>
 						{/each}
 					</div>
-					<div class="max-h-64 divide-y overflow-y-auto border border-[#E9B8FF]">
+					<div
+						style="background: linear-gradient(180deg, rgba(246, 245, 180, 0.08) 0%, rgba(243, 246, 245, 0) 100%);"
+						class="flex-1 divide-y overflow-y-auto border border-[#E9B8FF]"
+					>
 						{#each { length: 99 } as _, i}
 							<div class="relative flex items-center space-x-2.5 border-[#E9B8FF] p-2.5">
 								<div
-									class="absolute left-0 top-0 size-4 content-center bg-[#E9B8FF] text-center text-xs text-black"
+									class="absolute left-0 top-0 content-center bg-[#E9B8FF] px-1 py-0.5 text-center text-xs text-black"
 								>
 									<p>{i + 1}</p>
 								</div>
@@ -149,7 +273,100 @@
 				</div>
 			</div>
 		</section>
-		<section class="flex-[70%] border border-[#475D50] bg-[#475D50]/15">w</section>
+		<section
+			class="relative flex flex-[70%] flex-col overflow-hidden border border-[#475D50] bg-[#475D50]/15"
+		>
+			<div class="pointer-events-none absolute -bottom-6 -right-6 opacity-50">
+				{@render wordBombIcon({
+					width: undefined,
+					height: undefined,
+					class: 'w-[calc(min(45vw,60vh))] aspect-[901/916] mix-blend-color-dodge'
+				})}
+			</div>
+			<div class="absolute left-0 top-0 w-min text-nowrap rounded-br-2xl bg-[#475D50] px-3 py-1">
+				<p>Ready Players</p>
+			</div>
+			<div class="absolute right-3 top-3 w-min text-nowrap text-[#62E297]">
+				<p>97 slots left</p>
+			</div>
+			<div class="flex flex-1 items-center justify-center gap-x-20">
+				<div class="relative size-[304px]">
+					<div
+						in:fly|global={{ x: 48, y: 48, duration: 400, delay: 150 }}
+						out:fly|global={{ x: 48, y: -48, duration: 400 }}
+						class={[
+							readyPlayers.length === 0 ? 'delay-400 opacity-100' : 'opacity-0',
+							'absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center gap-y-4 text-[#B1C1AE] transition-opacity'
+						]}
+					>
+						<div
+							class="striped aspect-square size-24 content-center rounded-full border border-dashed text-center"
+						>
+							?
+						</div>
+						<p class="loading animate-pulse text-nowrap">Waiting for someone to ready</p>
+					</div>
+					{#each readyPlayers as uuid, i (uuid)}
+						{@const angleBetween = (2 * Math.PI) / readyPlayers.length}
+						{@const angle = i * angleBetween + Math.PI / 2}
+						{@const distanceFromCenter = 80 + Math.log2(readyPlayers.length) * 20}
+						{@const x = Math.cos(angle) * distanceFromCenter}
+						{@const y = Math.sin(angle) * distanceFromCenter - 32}
+						<div
+							in:fly|global={{ x: 48, y: 48, duration: 400, delay: 150 }}
+							out:fly|global={{ x: 48, y: -48, duration: 400 }}
+							style={readyPlayers.length === 1
+								? `translate: -50% -50%;`
+								: `translate: calc(-50% + ${x}px) calc(-50% + ${-y}px); scale: ${100 - Math.log2(readyPlayers.length) * 8}%; transition-timing-function: cubic-bezier(0.33, 1, 0.68, 1);`}
+							class="absolute left-1/2 top-1/2 flex flex-col items-center gap-y-2 transition-transform duration-[400ms]"
+						>
+							<img
+								src={`https://avatar.vercel.sh/${i}`}
+								alt="avatar"
+								width="120"
+								height="120"
+								class="size-24 rounded-full"
+							/>
+							<p>skeary</p>
+						</div>
+					{/each}
+				</div>
+				{@render doubleRightArrowIcon()}
+				<button
+					style="border-image: linear-gradient(to right, #475D50, #95C3A8) 1;"
+					class="z-10 w-72 space-y-2.5 border p-4 pt-5 backdrop-blur-sm backdrop-brightness-90 transition-all"
+				>
+					<div class="flex items-center justify-between">
+						<h1 style="font-family: 'PP Editorial New';" class="text-4xl text-[#F6F5B4]">
+							Word Bomb
+						</h1>
+						{@render downArrowIcon()}
+					</div>
+					<div class="h-[1px] w-full bg-[#475D50]"></div>
+					<div class="grid grid-cols-2 justify-between text-sm">
+						<p class="text-left font-medium text-[#8BA698]">Difficulty</p>
+						<p class="text-right text-[#8BA698]">Easy</p>
+						<p class="text-left font-medium text-[#8BA698]">Starting Lives</p>
+						<p class="text-right text-[#8BA698]">2</p>
+					</div>
+				</button>
+			</div>
+			<div class="z-10 flex gap-x-4 p-4">
+				<button
+					style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)"
+					class="flex-1 border border-[#E9B8FF] bg-[#161A15] p-4 text-xl text-[#E9B8FF] transition-all active:translate-y-1.5 active:bg-[#E9B8FF] active:text-[#161A15]"
+					onclick={() => readyPlayers.push(crypto.randomUUID())}
+				>
+					Ready
+				</button>
+				<button
+					style="box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)"
+					class="flex-1 border border-[#C0E8FF] bg-[#161A15] p-4 text-xl text-[#C0E8FF] transition-all active:translate-y-1.5 active:bg-[#C0E8FF] active:text-[#161A15]"
+				>
+					Start Early
+				</button>
+			</div>
+		</section>
 	</div>
 </main>
 
