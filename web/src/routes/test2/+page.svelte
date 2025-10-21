3c8f8bce1c890c5676d36aed554f85191aa9c043
skearya <77034153+skearya@users.noreply.github.com>
2025-10-21 01:34:49 -0400

    slightly more responsive

--- web/src/routes/test2/+page.svelte
+++ web/src/routes/test2/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
 	import type { SVGAttributes } from 'svelte/elements';
-	import { getAbortSignal } from 'svelte';
+	import { getAbortSignal, onMount } from 'svelte';
 	import { flip } from 'svelte/animate';
 	import { blur, fade, fly, scale, slide } from 'svelte/transition';
 	import crownSvg from '$lib/assets/crown.svg';
@@ -12,6 +12,8 @@
 	let { data }: PageProps = $props();
 
 	let activeOutlineElement: HTMLElement;
+	let playersContainer: HTMLElement;
+	let bombElement: HTMLElement;
 	let playerElements = $state<Record<string, HTMLElement>>({});
 	let arrowElements = $state<HTMLElement[]>([]);
 
@@ -19,7 +21,7 @@
 	let readyPlayers = $state<string[]>([crypto.randomUUID(), crypto.randomUUID()]);
 	let activePlayer = $state(0);
 
-	$effect(() => {
+	onMount(() => {
 		const intervalId = setInterval(() => {
 			unreadMessages += 1;
 		}, 1500);
@@ -53,6 +55,30 @@
 		const outlineX = outlineBBox.left + outlineBBox.width / 2;
 		const outlineY = outlineBBox.top + outlineBBox.height / 2;
 
+		const playerDistanceX = playerX - window.innerWidth / 2;
+		const playerDistanceY = playerY - window.innerHeight / 2;
+
+		console.log(playersContainer.style.translate);
+
+		const [prevContainerTransformX, prevContainerTransformY] = (
+			playersContainer.style.translate || '0px 0px'
+		)
+			.split(' ')
+			.map((value) => value.substring(0, value.length - 2))
+			.map((value) => parseFloat(value));
+
+		const containerTransformX = -playerDistanceX * 0.008;
+		const containerTransformY = -playerDistanceY * 0.008;
+
+		const bombTransformX = -playerDistanceX * 0.005;
+		const bombTransformY = -playerDistanceY * 0.005;
+
+		playersContainer.style.translate = `${containerTransformX}px ${containerTransformY}px`;
+		bombElement.style.translate = `calc(-50% + ${bombTransformX}px) calc(-50% + ${bombTransformY}px)`;
+
+		const newPlayerX = playerX + containerTransformX - prevContainerTransformX;
+		const newPlayerY = playerY + containerTransformY - prevContainerTransformY;
+
 		const toX1 = lerp(outlineX, playerX, 0.1);
 		const toY1 = lerp(outlineY, playerY, 0.1);
 
@@ -70,11 +96,11 @@
 				}
 			)
 			.finished.then(() => {
-				const fromX2 = lerp(outlineX, playerX, 0.9);
-				const fromY2 = lerp(outlineY, playerY, 0.9);
+				const fromX2 = lerp(outlineX, newPlayerX, 0.9);
+				const fromY2 = lerp(outlineY, newPlayerY, 0.9);
 
-				const toX3 = playerX;
-				const toY3 = playerY;
+				const toX3 = newPlayerX;
+				const toY3 = newPlayerY;
 
 				activeOutlineElement.animate(
 					{
@@ -92,19 +118,16 @@
 					}
 				);
 			});
-	});
 
-	$effect(() => {
 		const arrowElement =
 			arrowElements[activePlayer - 1 === -1 ? readyPlayers.length - 1 : activePlayer - 1];
 
 		(arrowElement.firstChild as HTMLElement).animate(
 			{
-				translate: [`0px 0px`, `0px 40px`],
+				translate: `0px 32px`,
 				opacity: ['100%', '0%']
 			},
 			{
-				fill: 'forwards',
 				easing: 'ease-out',
 				duration: 450
 			}
@@ -124,13 +147,13 @@
 		</feTurbulence>
 		<feComposite operator="in" in="turbulence" in2="SourceAlpha" result="composite" />
 		<feBlend in="SourceGraphic" in2="composite" mode="normal" result="blended" />
-		<feDisplacementMap in="blended" in2="turbulence" scale="8" />
+		<feDisplacementMap in="blended" in2="turbulence" scale="10" />
 	</filter>
 </svg>
 
 <main
 	style="background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(246, 245, 180, 0.08) 100%), #040605"
-	class="flex h-screen flex-col overflow-y-hidden"
+	class="relative flex h-screen overflow-hidden"
 >
 	<nav class="hidden items-center justify-between px-5 py-4">
 		{@render wordplayLogo()}
@@ -154,8 +177,11 @@
 			{@render settingsIcon()}
 		</div>
 	</nav>
-	<div class="flex flex-1 items-center justify-center gap-4 overflow-y-hidden p-4 pt-0">
-		<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+	<div class="flex flex-1 items-center justify-center gap-4 overflow-hidden p-4 pt-0">
+		<div
+			bind:this={bombElement}
+			class="timing-function-0 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-[400ms]"
+		>
 			{@render bombIcon()}
 			<div class="absolute right-0 top-0 -translate-y-[4.8rem] translate-x-[4.8rem]">
 				{@render bombWireIcon()}
@@ -170,24 +196,21 @@
 		<div bind:this={activeOutlineElement} class="absolute left-0 top-0">
 			<div
 				style={`scale: ${100 - Math.log2(readyPlayers.length) * 8}%;`}
-				class="rotating-border size-48 transition-transform duration-[400ms]"
+				class="rotating-border size-48"
 			></div>
 		</div>
-		<div class="relative size-[304px]">
+		<div
+			bind:this={playersContainer}
+			class="timing-function-0 relative size-full transition-transform duration-[400ms]"
+		>
 			{#each readyPlayers as uuid, i (uuid)}
-				{@const angleBetween = (2 * Math.PI) / readyPlayers.length}
-				{@const angle = i * angleBetween}
-				{@const distanceFromCenter = 328}
-				{@const x = Math.cos(angle) * distanceFromCenter}
-				{@const y = Math.sin(angle) * distanceFromCenter}
 				<div
 					bind:this={playerElements[uuid]}
-					in:fly={{ x: 48, y: 48, duration: 400, delay: 150 }}
-					out:fly={{ x: 48, y: -48, duration: 400 }}
-					style={`
-						translate: calc(-50% + ${x}px) calc(-50% + ${-y}px);
-						scale: ${100 - Math.log2(readyPlayers.length) * 8}%;
-					`}
+					style={`--angle-between: 2 * pi / ${readyPlayers.length};
+							--angle: ${i} * var(--angle-between);
+							--dist: min(100vw, 100vh) * 0.35;
+							translate: calc(-50% + cos(var(--angle)) * var(--dist)) calc(-50% - sin(var(--angle)) * var(--dist));
+							scale: ${100 - Math.log2(readyPlayers.length) * 8}%;`}
 					class="timing-function-0 absolute left-1/2 top-1/2 flex flex-col items-center p-2 transition-transform duration-[400ms]"
 				>
 					<div class="relative mb-2">
@@ -220,18 +243,14 @@
 			{#each readyPlayers as uuid, i (uuid)}
 				{@const angleBetween = (2 * Math.PI) / readyPlayers.length}
 				{@const angle = i * angleBetween + angleBetween / 2}
-				{@const distanceFromCenter = 328}
-				{@const x = Math.cos(angle) * distanceFromCenter}
-				{@const y = Math.sin(angle) * distanceFromCenter}
 				<div
 					bind:this={arrowElements[i]}
-					in:fly={{ x: 48, y: 48, duration: 400, delay: 150 }}
-					out:fly={{ x: 48, y: -48, duration: 400 }}
-					style={`
-						translate: calc(-50% + ${x}px) calc(-50% + ${-y}px);
-						scale: ${100 - Math.log2(readyPlayers.length) * 8}%;
-						rotate: ${(2 * Math.PI) / 2 - angle}rad;
-					`}
+					style={`--angle-between: 2 * pi / ${readyPlayers.length};
+							--angle: ${i} * var(--angle-between) + var(--angle-between) / 2;
+							--dist: min(100vw, 100vh) * 0.35;
+							translate: calc(-50% + cos(var(--angle)) * var(--dist)) calc(-50% - sin(var(--angle)) * var(--dist));
+							scale: ${100 - Math.log2(readyPlayers.length) * 8}%;
+							rotate: ${Math.PI - angle}rad;`}
 					class="timing-function-0 absolute left-1/2 top-1/2 text-transparent transition-transform duration-[400ms]"
 				>
 					<div class="opacity-0">
---
+++ web/src/routes/test3/+page.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let target: HTMLElement;
+
+	onMount(() => {
+		const clone = target.cloneNode(true) as HTMLElement;
+		clone.style.clipPath = 'polygon(0 0, 50% 0, 50% 50%, 0 50%)';
+
+		target.parentElement!.appendChild(clone);
+		clone.animate(
+			{
+				translate: 'calc(-50% + -40px) calc(-50% + -40px)'
+			},
+			{
+				fill: 'forwards',
+				easing: 'ease-out',
+				duration: 200
+			}
+		);
+	});
+</script>
+
+{#snippet heartIcon()}
+	<svg width="26" height="22" viewBox="0 0 25 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path
+			d="M0 7.13885C0 13.0709 5.02429 16.2321 8.70216 19.0615C10 20.06 11.25 21 12.5 21C13.75 21 15 20.06 16.2979 19.0615C19.9757 16.2321 25 13.0709 25 7.13885C25 1.20675 18.1248 -3.00018 12.5 2.70287C6.8752 -3.00018 0 1.20675 0 7.13885Z"
+			fill="#DA5858"
+		/>
+	</svg>
+{/snippet}
+
+<div
+	bind:this={target}
+	class="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center border p-2"
+>
+	<div class="relative mb-2">
+		<img
+			src={`https://avatar.vercel.sh/${0}`}
+			alt="avatar"
+			width="120"
+			height="120"
+			class="size-24 rounded-full"
+		/>
+		<div class="absolute bottom-0 left-0 flex flex-col gap-y-1.5 mix-blend-plus-lighter">
+			{@render heartIcon()}
+			{@render heartIcon()}
+		</div>
+	</div>
+	<p>skeary</p>
+	<p>ovens</p>
+</div>
